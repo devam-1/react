0a871107f44e8093c611b10e999c289718e92fa2
devam-1 <112586255+devam-1@users.noreply.github.com>
2022-08-31 21:34:10 +0100

    Add files via upload

---
+++ About.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+function About() {
+    return <div>
+        <center> <h2>Devam's page</h2>
+    <p> hello! my name is devam desai, and i'm a thirteen year old boy, who does web development</p>
+    <h4> i've created this by using node.js.</h4>
+    <div> some things that i like are : 
+        SPOTIFY! <h3> Spotify Free lets you listen to music at normal (96 kilobits per second) or high quality (160 Kbps), and you can't adjust the bitrate beyond that. Spotify Premium adds Very High quality streaming at 320 Kbps, which can make for better, more detailed audio output if you use high-end headphones or speakers.</h3>
+            YOUTUBE!  <a href="https://snobmonkey.com/the-advantages-and-disadvantages-of-youtube/"> free to use!</a>
+    </div>
+    </center>
+    
+    </div>;
+    
+}
+
+export default About;
